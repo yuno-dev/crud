088c98633971de9232c544a05bdd4bcbc1083be2
zWexlordy <68756210+yuno-dev@users.noreply.github.com>
2024-11-14 23:48:39 +0400

    Delete crud directory

--- crud/src/main/java/com/demo9/crud/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo9.crud.repository;
-
-import com.demo9.crud.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>  {
-}
